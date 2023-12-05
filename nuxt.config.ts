13a1545c29f4b0289e032d3dc956ac1efe3ea45f
Hardart <kaino@bk.ru>
2023-12-06 02:17:52 +0300

    add prerender page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,14 +11,15 @@ export default defineNuxtConfig({
     '/about': { prerender: true },
     '/contacts': { prerender: true },
     '/doctors': { prerender: true },
+    '/service': { prerender: true },
   },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', 'nuxt-delay-hydration', '@morev/vue-transitions/nuxt'],
 
   delayHydration: {
     // enables nuxt-delay-hydration in dev mode for testing
     // NOTE: you should disable this once you've finished testing, it will break HMR
-    debug: process.env.NODE_ENV === 'development',
-    // debug: false,
+    // debug: process.env.NODE_ENV === 'development',
+    debug: false,
     mode: 'init',
   },
 })
