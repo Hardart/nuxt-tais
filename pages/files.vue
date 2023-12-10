1de14c8062fcadc6ae510ad9c5247df64e8d157a
Hardart <kaino@bk.ru>
2023-12-11 01:36:22 +0300

    add package sharp

--- pages/files.vue
+++ pages/files.vue
@@ -6,7 +6,6 @@ const { data: images } = await useFetch('/api/files')
   <Section>
     <ul class="grid grid-cols-4 gap-5">
       <li v-for="image in images"><img :src="image" alt="" width="300" height="300" /></li>
-      <!-- <li><img src="~/assets/images/avatar.jpeg" alt="" /></li> -->
     </ul>
   </Section>
 </template>
