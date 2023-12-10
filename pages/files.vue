e17bf68d992454daf5e8680676e15f91cbb03ab0
Hardart <kaino@bk.ru>
2023-12-10 23:19:20 +0300

    wrap images to section

--- pages/files.vue
+++ pages/files.vue
@@ -3,12 +3,12 @@ const { data: images } = await useFetch('/api/files')
 </script>
 
 <template>
-  <div>
+  <Section>
     <ul class="grid grid-cols-4 gap-5">
       <li v-for="image in images"><img :src="image" alt="" width="300" height="300" /></li>
       <!-- <li><img src="~/assets/images/avatar.jpeg" alt="" /></li> -->
     </ul>
-  </div>
+  </Section>
 </template>
 
 <style scoped></style>
