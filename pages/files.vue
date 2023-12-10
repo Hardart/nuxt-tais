cb55772a5fa33305e90854ba2f95752eac13a621
Hardart <kaino@bk.ru>
2023-12-10 23:11:34 +0300

    show image storage

---
+++ pages/files.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { data: images } = await useFetch('/api/files')
+</script>
+
+<template>
+  <div>
+    <ul class="grid grid-cols-4 gap-5">
+      <li v-for="image in images"><img :src="image" alt="" width="300" height="300" /></li>
+      <!-- <li><img src="~/assets/images/avatar.jpeg" alt="" /></li> -->
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
