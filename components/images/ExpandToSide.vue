525ffc512649d89829b040b70e6bfb128a9896c6
Hardart <kaino@bk.ru>
2024-01-08 17:49:08 +0300

    small fix

--- components/images/ExpandToSide.vue
+++ components/images/ExpandToSide.vue
@@ -2,12 +2,13 @@
 defineProps<{
   image: string
 }>()
+const shape = '/images/shapes/home-shape-01.svg'
 </script>
 
 <template>
   <div class="relative">
     <div class="absolute -left-[170px] max-xl:hidden">
-      <img src="/images/shapes/home-shape-01.svg" width="170" height="170" />
+      <img :src="shape" width="170" height="170" />
     </div>
     <div class="lg:-mr-[40px]">
       <img :src="`/images/${image}`" width="1510" height="885" loading="eager" fetchpriority="high" decoding="async" />
